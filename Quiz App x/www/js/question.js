f9385b11ba805cc745890e38db0f1233e20f6015
ej <ej@gmail.ph>
2017-12-05 12:32:26 -0800

    ito na

---
+++ Quiz App x/www/js/question.js
@@ -0,0 +1,76 @@
+ var questions = [{
+ 	"question": "The first mechanical computer designed by charles babbage was called ?",
+ 	"option1": "abacus",
+ 	"option2": "analytical engine",
+ 	"option3": "calculator",
+ 	"option4": "ball",
+ 	"answer":"2"
+
+}, {
+	"question": "How to install react app?",
+ 	"option1": "$ npm install -g cordova ",
+ 	"option2": "$ npm install -j create-project-app",
+ 	"option3": "$ npm install -g create-react-app",
+ 	"option4": "$ npm install -g create-app-react",
+ 	"answer":"3"
+}, {
+	"question": "How to create react-app?",
+	"option1": "$ create-react-app my-app-name",
+	"option2": "$ create-cordova-app my-app-name",
+	"option3": "$ create-apache-npm my-app name",
+	"option4": "$create-react-app",
+	"answer": "1"
+
+}, {
+	"question": "who invented c?",
+	"option1": "dennis",
+	"option2": "Dennis Ritchie",
+	"option3": "denistro ritchies",
+	"option4": "google chrome",
+	"answer": "2"
+
+}, {
+	"question": "In what year was Google launched on the web?",
+	"option1": "1997",
+	"option2": "2017",
+	"option3": "1999",
+	"option4": "1998",
+	"answer": "4"
+}, {
+	"question": "In computing what is Ram short for?",
+	"option1": "Random Access Memory",
+	"option2": "Random Acadimics Memory",
+	"option3": "Random Access Member",
+	"option4": "Random Accounting Memory",
+	"answer": "1"
+}, {
+	"question": "Which unit is an indication for the sound quality of MP3?",
+	"option1": "mmppbbss",
+	"option2": "ps",
+	"option3": "mbps",
+	"option4": "Kbps",
+	"answer": "4"
+}, {
+	"question": "What is the name of the network of computers from which the Internet has emerged?",
+	"option1": "Arpanetshaker",
+	"option2": "Arplanet",
+	"option3": "Arpanet",
+	"option4": "Arpanetas",
+	"answer": "3"
+}, {
+	"question": "What is the country top-level domain of Belgium?",
+	"option1": "who .is domain",
+	"option2": "The .be domain",
+	"option3": "The .be a domainating",
+	"option4": "domain only",
+	"answer": "1"
+}, {
+	"question": "How many oscars did the Titanic movie got?",
+	"option1": "12",
+	"option2": "13",
+	"option3": "14",
+	"option4": "11",
+	"answer": "4"
+}];
+
+
